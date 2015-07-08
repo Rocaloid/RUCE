417664dcbf57b225ccb4fe32a6b1699ca247e159
Star Brilliant <m13253@hotmail.com>
2015-07-08 23:13:41 +0800

    It works!

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
+/*
+  Rocaloid
+  Copyright (C) 2015 StarBrilliant <m13253@hotmail.com>
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
 #include <libwintf8.h>
 
 int main() {
-    WTF8::cout << "It works!" << std::endl;
+    WTF8::cout << "Rocaloid test program, version Nanodesu" << std::endl
+               << std::endl
+               << "It works!" << std::endl
+               << "能用了的说！" << std::endl
+               << "稼働できるなのです！" << std::endl
+               << std::endl;
     return 0;
 }
