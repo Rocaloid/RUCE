472155c9c9bec5f79466a85cafd981bc8eb81b6c
Star Brilliant <m13253@hotmail.com>
2015-07-09 19:30:11 +0800

    Add CmdlineParser OptionManager

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,34 @@
 /*
   Rocaloid
   Copyright (C) 2015 StarBrilliant <m13253@hotmail.com>
-  
+
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-  
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "cmdline-parser.hpp"
+#include "option-manager.hpp"
 #include <iostream>
-#include <libwintf8.h>
+#include <libwintf8/argv.h>
+#include <libwintf8/termio.h>
 
 int main() {
+    using namespace RUCE;
+
     WTF8::cerr << "Rocaloid synthesizer, version nanodesu" << std::endl << std::endl;
+
+    CmdlineParser cmdline_parser(WTF8::getargv());
+    OptionManager option_manager(cmdline_parser);
 
     return 0;
 }
