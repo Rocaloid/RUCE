2033d34afc34893b06fae26b7378bc3b6be673ad
Star Brilliant <m13253@hotmail.com>
2015-07-19 14:28:43 +0800

    试一下调幅，效果不错

--- src/synthesizer.cpp
+++ src/synthesizer.cpp
@@ -72,17 +72,25 @@ void Synthesizer::synth_unit() {
     // STUB
 
     double pitch = option_manager.get_output_pitch();
-    double phrases[13] = { 0 };
+    double phrases[2][13] = { 0 };
     size_t i = 0;
     for(auto &s : p->buffer) {
         s = p->fastrand() / 16;
         double instant_pitch = pitch + option_manager.get_pitch_bend(double(i) / p->output_sample_rate);
         for(size_t j = 1; j < 13; j++) {
-            phrases[j] += 2.0 * j * p->tuner.midi_id_to_freq(instant_pitch + p->fastrand()*4 - 2);
+            static const double am_freq[13] = {
+                0, 1.9, 7.6, 3.8, 13.3, 15.4, 14.2, 14.1, 7.9, 7.6, 50.4, 10.7, 19.8
+            };
+            phrases[0][j] += 2.0 * j * p->tuner.midi_id_to_freq(instant_pitch + p->fastrand()*2 - 1) - am_freq[j];
+            phrases[1][j] += 2.0 * j * p->tuner.midi_id_to_freq(instant_pitch + p->fastrand()*2 - 1) + am_freq[j];
         }
-        static const double intensity[13] = { 0, 1, 1, 1, 1.0/8, 1.0/64, 1.0/64, 1.0/64, 1.0/8, 1.0/8, 1.0/8, 1.0/8, 1.0/64 };
+        static const double intensity[2][13] = {
+            { 0, 1.1/2, 1.2/2, 1.5/4, 1.3/16, 1.3/128, 1.3/128, 1.3/128, 1.2/16, 1.1/16, 1.2/16, 1.1/16, 1.2/128 },
+            { 0, 0.9/2, 0.8/2, 0.5/4, 0.3/16, 0.3/128, 0.3/128, 0.3/128, 0.8/16, 0.9/16, 0.8/16, 0.9/16, 0.8/128 }
+        };
         for(size_t j = 1; j < 13; j++) {
-            s += std::sin(M_PI * phrases[j] / p->output_sample_rate) * intensity[j];
+            s += std::sin(M_PI * phrases[0][j] / p->output_sample_rate) * intensity[0][j];
+            s += std::sin(M_PI * phrases[1][j] / p->output_sample_rate) * intensity[1][j];
         }
         s *= option_manager.get_note_volume()/8;
         i++;
