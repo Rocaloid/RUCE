0a7702c99782bb7a5bbc6287c0e6e26bd090371c
Star Brilliant <m13253@hotmail.com>
2015-08-03 23:58:12 +0800

    Synthesizer: First step to separate analysis step and synthesis step

---
+++ src/hnm-parameters.hpp
@@ -0,0 +1,56 @@
+/*
+    Rocaloid
+    Copyright (C) 2015 StarBrilliant <m13253@hotmail.com>
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 3.0 of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this program.  If not,
+    see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef RUCE_HNM_PARAMETERS_HPP
+#define RUCE_HNM_PARAMETERS_HPP
+
+#include <cstdint>
+#include <vector>
+#include "utils.hpp"
+#include "wrapped-angle.hpp"
+
+namespace RUCE {
+
+class HNMParameters {
+public:
+    ssize_t analysis_window_size;
+    ssize_t analysis_window_hop;
+    size_t max_pillars;
+    /**
+     * The middle point of the first analysis window
+     */
+    ssize_t first_window_mid;
+    /**
+     * The first index is window index,
+     * the second index is harmony index.
+     */
+    std::vector<std::vector<double>> harmony_frequencies;
+    std::vector<std::vector<double>> harmony_magnitude;
+    std::vector<std::vector<WrappedAngle>> harmony_phase;
+    double frame_to_window_idx(ssize_t frame_idx) const {
+        return double(frame_idx-first_window_mid) / double(analysis_window_hop);
+    }
+    ssize_t window_to_frame_idx(double window_idx) const {
+        return ssize_t(std::round(window_idx*analysis_window_hop)) + first_window_mid;
+    }
+};
+
+}
+
+#endif
---
+++ src/wrapped-angle.hpp
@@ -0,0 +1,97 @@
+/*
+    Rocaloid
+    Copyright (C) <YEAR> <YOUR NAME>
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 3.0 of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this program.  If not,
+    see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef RUCE_WRAPPED_ANGLE_HPP
+#define RUCE_WRAPPED_ANGLE_HPP
+
+// Define M_PI etc
+#ifndef _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
+#endif
+#include <cmath>
+
+namespace RUCE {
+
+class WrappedAngle {
+public:
+    WrappedAngle(double angle = 0) : angle(angle) {}
+    operator double () const {
+        double res = angle;
+        if(!std::isnormal(res)) {
+            res = NAN;
+        } if(angle > 16 || angle < -16) {
+            res = std::fmod(M_PI-res, 2*M_PI);
+            if(res < 0)
+                res += 2*M_PI;
+        } else if(res > M_PI) {
+            do {
+                res -= 2*M_PI;
+            } while(res > M_PI);
+        } else if(res <= M_PI) {
+            do {
+                res += 2*M_PI;
+            } while(res <= M_PI);
+        }
+        return res;
+    }
+    WrappedAngle &wrap() {
+        angle = (double) *this;
+        return *this;
+    }
+    WrappedAngle operator + (const WrappedAngle rhs) const {
+        return WrappedAngle(angle+rhs.angle);
+    }
+    WrappedAngle operator - (const WrappedAngle rhs) const {
+        return WrappedAngle(angle-rhs.angle);
+    }
+    WrappedAngle operator * (double rhs) const {
+        return WrappedAngle(double(*this) * rhs);
+    }
+    WrappedAngle operator / (double rhs) const {
+        return WrappedAngle(double(*this) / rhs);
+    }
+    WrappedAngle &operator += (const WrappedAngle rhs) {
+        angle += rhs.angle;
+        return *this;
+    }
+    WrappedAngle &operator -= (const WrappedAngle rhs) {
+        angle += rhs.angle;
+        return *this;
+    }
+    WrappedAngle &operator *= (double rhs) {
+        wrap();
+        angle *= rhs;
+        return *this;
+    }
+    WrappedAngle &operator /= (double rhs) {
+        wrap();
+        angle /= rhs;
+        return *this;
+    }
+    WrappedAngle &operator - () {
+        angle = -angle;
+        return *this;
+    }
+private:
+    double angle;
+};
+
+}
+
+#endif
